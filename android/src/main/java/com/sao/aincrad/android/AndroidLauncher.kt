2434c373b14b689b079c7d2acf94d63a9e683210
Ffenuss <78445717+Ffenuss@users.noreply.github.com>
2026-05-25 12:01:15 +0300

    Update AndroidLauncher.kt

--- android/src/main/java/com/sao/aincrad/android/AndroidLauncher.kt
+++ android/src/main/java/com/sao/aincrad/android/AndroidLauncher.kt
@@ -1,6 +1,8 @@
 package com.sao.aincrad.android
 
 import android.os.Bundle
+import java.io.PrintWriter
+import java.io.StringWriter
 import com.badlogic.gdx.backends.android.AndroidApplication
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
 import com.sao.aincrad.AincradGame
@@ -8,6 +10,7 @@ import com.sao.aincrad.AincradGame
 class AndroidLauncher : AndroidApplication() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installCrashHandler()
 
         val config = AndroidApplicationConfiguration().apply {
             useAccelerometer = false
@@ -16,6 +19,32 @@ class AndroidLauncher : AndroidApplication() {
             useWakelock = true
         }
 
-        initialize(AincradGame(), config)
+        try {
+            initialize(AincradGame(), config)
+        } catch (t: Throwable) {
+            showCrashScreen(t)
+            finish()
+        }
+    }
+
+    private fun installCrashHandler() {
+        val previous = Thread.getDefaultUncaughtExceptionHandler()
+        Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
+            showCrashScreen(throwable)
+            previous?.uncaughtException(thread, throwable)
+        }
+    }
+
+    private fun showCrashScreen(t: Throwable) {
+        val sw = StringWriter()
+        t.printStackTrace(PrintWriter(sw))
+        val report = buildString {
+            appendLine("SAO runtime crash")
+            appendLine()
+            appendLine("${t::class.java.name}: ${t.message ?: "no message"}")
+            appendLine()
+            append(sw.toString())
+        }
+        CrashActivity.open(this, report)
     }
 }
